98db881627b3a695f9812b49c1425f4e9beba5f0
DevenBhalerao <devenbhalerao@gmail.com>
2016-03-18 20:58:40 +0530

    added question and choice objects

--- Survey/views.py
+++ Survey/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
-
+from .models import Choice,Question
 # Create your views here.
+
 def index(request):
 	return render(request, "index.html", {})
 
 def survey(request):
-	return render(request, "Take_survey.html", {})
+	question_objects = Question.objects.all()
+	choice_objects = Choice.objects.all()
+	context = {
+		"questions":question_objects,
+		"choices" :choice_objects
+	}
+	return render(request, "Take_survey.html", context)
